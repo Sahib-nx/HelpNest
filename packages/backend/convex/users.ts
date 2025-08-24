abb8b5bda49404fac6419a096c400dbee28c0d56
Sahib-nx <sahibyar214@gmail.com>
2025-08-23 22:39:42 -0400

    04: organizations

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -17,6 +17,13 @@ export const add = mutation({
         if (identity === null) {
             throw new Error("Not authenticated");
         }
+
+        const orgId = identity.orgId as string;
+
+        if(!orgId) {
+            throw new Error("Misiing OrganizatioId")
+        };
+
         const userId = await ctx.db.insert("users", {
             name: "Sahib",
         });
